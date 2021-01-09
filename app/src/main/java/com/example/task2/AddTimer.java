345e90ff844614373849c0ee9346778edf6365b4
Adfferg <maxrusak01@gmail.com>
2021-01-09 16:56:40 +0300

    09.01.2021

---
+++ app/src/main/java/com/example/task2/AddTimer.java
@@ -0,0 +1,18 @@
+package com.example.task2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTimer extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_timer);
+    }
+
+}
--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.task2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button addTimerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addTimerButton = findViewById(R.id.addTimerButton);
+        addTimerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenNewActivity();
+            }
+        });
+    }
+    public void OpenNewActivity(){
+        startActivity(new Intent(MainActivity.this, AddTimer.class));
     }
 }
